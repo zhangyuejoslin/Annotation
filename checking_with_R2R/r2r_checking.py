6d8c8b9f72a76e93e027213061382b7bbe8114fd
zhangyuejoslin <949788345@qq.com>
2020-02-25 14:02:59 -0500

    check with one example

--- checking_with_R2R/r2r_checking.py
+++ checking_with_R2R/r2r_checking.py
@@ -17,7 +17,7 @@ def next_motion(motion_type, start_point, end_point):
         if end_point[0] > start_point[0] and abs(end_point[1] - start_point[1])<=0.3:
             return end_point
     if motion_type == "Motions:Right":
-        if end_point[0] > start_point[0] and abs(end_point[1] - start_point[1])<=0.3:
+        if end_point[0] > start_point[0] and end_point[1] < start_point[1] and abs(end_point[1] - start_point[1])>0.3:
             return end_point
 
 def get_related_path(start_point):
@@ -26,10 +26,17 @@ def get_related_path(start_point):
             related_image = image['path']
     return  related_image
 
-def object_detection(distance_type, text_object, image_object):
-   if distance_type == "close":
+def object_text_image(text_object, image_object):
        if text_object in image_object:
            return True
+
+def next_configuration(distance_type, text_object, image_object):
+    if distance_type == "far":
+        if text_object not in image_object:
+            return True
+    if distance_type == "close":
+        if text_object in image_object:
+            return True
 
 def get_object(image_id):
     for image in image_data:
@@ -37,32 +44,41 @@ def get_object(image_id):
             related_objects = image['objects']
     return  related_objects
 
-
 def path_checking(start_point, configure):
     next_point = start_point
     path = []
     path.append(start_point)
     for config_num, config_content in configure.items():
+        if config_content['type'] == "Motions:Stop":
+            break
+
         while True:
-            # one step
-            for each_related_image in get_related_path(next_point):
-                if next_motion(config_content['type'], get_pose(next_point), get_pose(each_related_image)):
-                    print(config_num)
-                    next_point = each_related_image
-                    path.append(next_point)
-                    print(path)
-                    break
+
+            # if it is forward and you have seen the objective, then move,
+            # if it is forward and you have not seen the objective, then don't move
+            # if it is other direction and you have not seen the objective, then move
+          if config_content['type']!= "Motions:Forward" or object_text_image(config_content['spatial_entity']['SPL' + config_num[6:]]['text'],get_object(next_point)):
+            for each_related_path in get_related_path(next_point):
+                    if next_motion(config_content['type'], get_pose(next_point), get_pose(each_related_path)):
+                        next_point = each_related_path
+                        path.append(next_point)
+                        break
 
             # if achieve the objective of the configuration
-            if not object_detection(config_content['distance'],config_content['spatial_entity']['SPL'+config_num[6:]]['text'],get_object(each_related_image)):
+            if next_configuration(config_content['distance'],config_content['spatial_entity']['SPL'+config_num[6:]]['text'],get_object(each_related_path)):
+
                 break
+          else:
+              break
     print(path)
+
 
 if __name__ == '__main__':
 
 
      new_iamge_representation = []
      for configure in text_data:
+
         start_point = "10c252c90fa24ef3b698c6f54d984c5c"
         path_checking(start_point, configure)
         break
