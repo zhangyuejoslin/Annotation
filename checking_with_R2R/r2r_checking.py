3bde958ee8435c891e11ece8018be72f69d088b8
zhangyuejoslin <949788345@qq.com>
2020-03-02 17:07:06 -0500

    the second example

--- checking_with_R2R/r2r_checking.py
+++ checking_with_R2R/r2r_checking.py
@@ -12,23 +12,30 @@ def get_pose(image_id):
             if image['image_id'] == image_id:
                 return image['pose']
 
-def next_motion(motion_type, start_id, end_id):
+def next_motion(motion_type, start_id, end_id, direction):
     start_point = get_pose(start_id)
     end_point = get_pose(end_id)
-
-    if motion_type == "Motions:Forward":
-        if end_point[0] > start_point[0] and abs(end_point[1] - start_point[1])<=0.5:
-            return end_id
-    elif motion_type == "Motions:Right":
-        if end_point[0] > start_point[0] and end_point[1] < start_point[1] and abs(end_point[1] - start_point[1])>0.5:
-            return end_id
-    elif motion_type == "Motions:Left":
-        if end_point[0] > start_point[0] and end_point[1] > start_point[1] and abs(end_point[1] - start_point[1]) > 0.5:
-            return end_id
-    elif motion_type == "Motions:Turn Right":
-        return start_id
-    else:
-        return None
+    if direction == 1.5:
+        if motion_type == "Motions:Forward":
+            if end_point[0] > start_point[0] and abs(end_point[1] - start_point[1])<=0.5:
+                return end_id
+        elif motion_type == "Motions:Right":
+            if end_point[0] > start_point[0] and end_point[1] < start_point[1] and abs(end_point[1] - start_point[1])>0.5:
+                return end_id
+        elif motion_type == "Motions:Left":
+            if end_point[0] > start_point[0] and end_point[1] > start_point[1] and abs(end_point[1] - start_point[1]) > 0.5:
+                return end_id
+        elif motion_type == "Motions:Turn Right":
+            return start_id
+        else:
+            return None
+    elif direction == 3.0:
+        if motion_type == "Motions:Forward":
+         if end_point[1] < start_point[1] and abs(end_point[0] - start_point[0])<=0.5:
+                return end_id
+        elif motion_type == "Motions:Left":
+            if end_point[1] < start_point[1] and end_point[0] > start_point[0] and abs(end_point[0] - start_point[0])>0.5:
+                return end_id
 
 def get_related_path(start_point):
     for image in image_data:
@@ -40,30 +47,48 @@ def object_text_image(text_object, image_object):
        if text_object in image_object:
            return True
 
-def next_configuration(distance_type, text_object, image_object):
+
+def next_configuration(distance_type, text_object, type,direction, path):
+    image_object = get_object(direction, path)
     if distance_type == "far":
         if text_object not in image_object:
             return True
     if distance_type == "close":
         if text_object in image_object:
             return True
+        if type == "Motions:Right":
+            direction+= 1.5
+            image_object= get_object(direction,path)
+            if text_object in image_object:
+                return True
 
-def get_object(image_id):
-    for image in image_data:
-        if image["image_id"] == image_id:
-            related_objects = image['objects']
-    return  related_objects
+
+def get_object(direction, image_id):
+        related_object = []
+        for image in image_data:
+            if image["image_id"] == image_id:
+                for each_object in image['objects'].items():
+                    if str(direction) in each_object[1]:
+                        related_object.append(each_object[0])
+        return related_object
 
 def next_direction(direction, motion_type):
-    # 0 is north, 90 is east, 180 is south, and 270 is west.
     if motion_type == "Motions:Forward":
             return direction
-    elif motion_type == "Motions:Right" or "Motions:Turn Right":
-            direction += 30
+    elif  motion_type == "Motions:Right" or motion_type == "Motions:Turn Right":
+            direction += 1.5
             return direction
-    elif motion_type == "Motions:Left" or "Motions:Turn Right":
-            direction -= 30
+    elif motion_type == "Motions:Left" or motion_type == "Motions:Turn Left":
+            direction -= 1.5
             return direction
+
+def second_viewer_for(direction, type, view_point, trajector):
+    if type == "Direction:Right":
+        direction += 1.5
+    elif type == "Direction:Left":
+        direction -= 1.5
+    if trajector in get_object(direction, view_point):
+        return True
 
 def path_checking(start_point, start_direction, configure):
     next_point = start_point
@@ -71,44 +96,48 @@ def path_checking(start_point, start_direction, configure):
     direction_of_path = []
     path = []
     path.append(start_point)
-    direction_of_path.append(direction)
     for config_num, config_content in configure.items():
-
         if config_content['type'] == "Motions:Stop":
             break
 
-        if "Motions" not in config_content['type']:
-            continue
+        if "Motions" not in config_content["type"]:
+            if config_content.get("For") == "second":
+                if second_viewer_for(direction,config_content["type"], next_point, config_content["spatial_entity"]["SPT" + config_num[6:]]["text"]):
+                    continue
+                else:
+                    break
+            else:
+                continue
 
         while True:
 
             # if it is forward and you have seen the objective, then move,
             # if it is forward and you have not seen the objective, then don't move
             # if it is other direction and you have not seen the objective, then move
-
-            # turn right to the hallway and if you go forward , you must make sure that you can see something than you move
+            # if you go forward , you must make sure that you can see something than you move
+            # if you want to change direction,
             if 'SPL' + config_num[6:] not in config_content['spatial_entity'] \
-                  or object_text_image(config_content['spatial_entity']['SPL' + config_num[6:]]['text'],get_object(next_point)) \
-                  or config_content['type']!= "Motions:Forward":
-
+                  or object_text_image(config_content['spatial_entity']['SPL' + config_num[6:]]['text'],get_object(direction, next_point)) or \
+                     config_content['type'] != "Motions:Forward":
                 for each_related_path in get_related_path(next_point):
-                    next_motion_point = next_motion(config_content['type'], next_point, each_related_path)
+                    next_motion_point = next_motion(config_content['type'], next_point, each_related_path, direction)
+
                     if next_motion_point is None:
                         continue
                     if next_motion_point != next_point:
                         next_point = each_related_path
-                    direction = next_direction(direction, config_content['type'])
                     direction_of_path.append(direction)
                     path.append(next_point)
-                    #print(config_num+"$$$")
-
                     break
             # if achieve the objective of the configuration
                 if 'SPL' + config_num[6:] not in config_content['spatial_entity'] or \
-                next_configuration(config_content['distance'],config_content['spatial_entity']['SPL'+config_num[6:]]['text'],get_object(each_related_path)):
+                next_configuration(config_content['distance'],config_content['spatial_entity']['SPL'+config_num[6:]]['text'], config_content['type'], direction, each_related_path):
+                    direction = next_direction(direction, config_content['type'])
                     break
             else:
                 break
+
+    direction_of_path.append(direction)
     print(direction_of_path)
     print(path)
 
@@ -120,6 +149,13 @@ if __name__ == '__main__':
      for configure in text_data:
 
         start_point = "10c252c90fa24ef3b698c6f54d984c5c"
+        #start_point = "db145474a5fa476d95c2cc7f09e7c83a"
         # 0 is forward, 1 is right, 2 is left
-        start_direction = 90
+        start_direction = 1.5
         path_checking(start_point, start_direction, configure)
+        # break
+
+
+
+
+
